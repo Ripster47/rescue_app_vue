db72d3198b1d5afa226875ffd4723772ce4582a5
Ripster47 <mpolinski12@gmail.com>
2019-03-25 17:07:07 -0500

    create user page officially cssed

--- src/views/AnimalsIndex.vue
+++ src/views/AnimalsIndex.vue
@@ -1,17 +1,58 @@
 <template>
   <div class="animals-index">
-    <div v-for="animal in animals">
+    <div class="container">
+      <div class="card-deck row row-eq-height">
+        <div class="card col-md-4 " v-for="animal in animals">
+          <div class="card_img">
+            <router-link v-bind:to="'/animals/' + animal.id">
+              <img class="img-full" v-bind:src="animal.image_url" v-bind:alt="animal.name">
+            </router-link>
+          </div>
+          <div class="card-block">
+            <a href="#"><h3 class="card-title">{{animal.name}}</h3></a>
+            <p class="card-text">{{animal.description}}</p>
+            <a href="" class="btn btn-primary">Read more</a>
+          </div>
+        </div>
+      </div>
+    <!-- <div class="card">
+    <div class="card_img">
+      <router-link v-bind:to="'/animals/' + animal.id">
+        <img class="img-full" v-bind:src="animal.image_url" v-bind:alt="animal.name">
+      </router-link>
+    </div>
+    <div class="card-block">
+      <a href="#"><h3 class="card-title">{{animal.name}}</h3></a>
+      <p class="card-text">{{animal.description}}</p>
+      <a href="" class="btn btn-primary">Read more</a>
+    </div>
+    </div>
+        <div class="card">
+    <div class="card_img">
+      <router-link v-bind:to="'/animals/' + animal.id">
+        <img class="img-full" v-bind:src="animal.image_url" v-bind:alt="animal.name">
+      </router-link>
+    </div>
+    <div class="card-block">
+      <a href="#"><h3 class="card-title">{{animal.name}}</h3></a>
+      <p class="card-text">{{animal.description}}</p>
+      <a href="" class="btn btn-primary">Read more</a>
+    </div>
+    </div>
+  </div>
+   
+
       <h1>{{animal.name}}</h1>
       <router-link v-bind:to="'/animals/' + animal.id">
         <img v-bind:src="animal.image_url" v-bind:alt="animal.name">
-      </router-link>
+      </router-link> -->
     </div>
   </div>
 </template>
 
 <style>
-img{
-  width: 250px;
+.animals-index .card:hover {
+  box-shadow: 8px 8px 35px 8px whitesmoke
 }
 </style>
 
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,137 +1,158 @@
 <template>
   <div class="signup">
-    <div>
-      <form v-on:submit.prevent="submit()">
-        <h1>Create Your Profile!</h1>
-        <h3>Here, we will gather all of the information needed in order for you to adopt/relinquish an animal, from here forward you will be offered the chance to edit your information, but wont have to fill this out fully again.</h3>
+    <div class="container">
+    <form v-on:submit.prevent="submit()">
+
         <ul>
           <li v-for="error in errors">{{ error }}</li>
         </ul>
-        <div>
-          <label>First Name:</label> 
-          <input type="text" v-model="firstName">
+
+        <h1 class=" text-center pb-5">Create Your Profile</h1>
+        <p class="pb-5">Here, we will gather all of the information needed in order for you to adopt/relinquish an animal, from here forward you will be offered the chance to edit your information, but wont have to fill this out fully again</p>
+
+      <div class="row">
+        <div class="form-group col-md-6">
+          <input type="text" class="form-control" aria-describedby="userName" placeholder="First Name" v-model="firstName">
         </div>
-         <div>
-          <label>Last Name:</label> 
-          <input type="text" v-model="lastName">
+        <div class="form-group col-md-6">
+          <input type="text" class="form-control"  aria-describedby="userName" placeholder="Last Name" v-model="lastName">
         </div>
-        <div>
-          <label>Email:</label>
-          <input type="email" v-model="email">
+        <div class="form-group col-md-6">
+          <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Email" v-model="email">
         </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" v-model="password">
+        <div class="form-group col-md-6">
+          <input type="text" class="form-control"  placeholder="Phone" v-model="phoneNumber">
         </div>
-        <div>
-          <label>Password confirmation:</label>
-          <input type="password" v-model="passwordConfirmation">
+         <div class="form-group col-md-6">
+          <input type="password" class="form-control" aria-describedby="emailHelp" placeholder="Password" v-model="password">
         </div>
-        <div>
-          <label>Phone Number:</label>
-          <input type="text" v-model="phoneNumber" placeholder="XXXXXXXXXX"> (Just numbers, no symbols!) 
+        <div class="form-group col-md-6">
         </div>
-        <div>
-          <label>Address 1:</label>
-          <input type="text" v-model="address1">
+         <div class="form-group col-md-6">
+          <input type="password" class="form-control" aria-describedby="emailHelp" placeholder="Password Confirmation" v-model="passwordConfirmation">
         </div>
-         <div>
-          <label>Address 2:</label>
-          <input type="text" v-model="address2">
+         <div class="form-group col-md-6">
         </div>
-        <div>
-          <label>City:</label>
-          <input type="text" v-model="city">
+        <div class="form-group col-md-6">
+          <input type="text" class="form-control"  placeholder="Home Address" v-model="address1">
         </div>
-        <div>
-          <label>State:</label>
-          <input type="text" v-model="state">
+        <div class="form-group col-md-6">
+          <input type="text" class="form-control" placeholder="Apt #, Suite, Etc." v-model="address2">
         </div>
-        <div>
-          <label>Zip Code:</label>
-          <input type="text" v-model="zipCode">
+        <div class="form-group col-md-6">
+          <input type="text" class="form-control" placeholder="City" v-model="city">
         </div>
-          <h4>Your Home</h4>
-        <div>
-          <label>What type of residence are you currently occupying?</label>
-          <select name="accommodations" id="accommodations" v-model="accommodations">
-            <option>Apartment</option>
-            <option>Condominium</option>
-            <option>House</option>
-            <option>Other</option>
-          </select>
+        <div class="form-group col-md-6">
+          <input type="text" class="form-control" placeholder="State Ex. NY, MA, FL" v-model="state">
         </div>
-        <div>
+        <div class="form-group col-md-6">
+          <input type="text" class="form-control" placeholder="Zip Code Ex 90210" v-model="zipCode">
         </div>
-        <div>
+         <div class="form-group col-md-6">
+        </div>
+          <h4 class="col-12 text-center pb-2">Residence Information</h4>
+        <div class="col-12">
+          <label for="example-location" class="col-md-6 ">What type of residence are you currently occupying?</label>
+            <div class="col-md-2 selectOptions ">
+              <select name="country" class="form-control select-drop" v-model="accommodations">
+                <option>Apartment</option>
+                <option>Condominium</option>
+                <option>House</option>
+                <option>Other</option>
+              </select>
+            </div>
+        </div>
+        <div class="col-12 form-check">
           <label>If you rent, will you be able to provide proof of your landlord’s approval or rental agreement stating it is allowable to have a pet?</label><br>
-          <input type="radio" name="rent" value="true" v-model="rent">Yes<br>
-          <input type="radio" name="rent" value="false" v-model="rent">No<br>
-          <input type="radio" name="rent" value="false">I do not rent<br>
-        </div>
-        <div>
+            <input  class="radio-custom form-check-input" name="radio-group-rent" type="radio" value="true" v-model="rent">
+            <label  class="radio-custom-label form-check-label">Yes</label>
+            <input  class="radio-custom form-check-input" name="radio-group-rent" type="radio" value="false" v-model="rent">
+            <label  class="radio-custom-label pl-2 form-check-label">No</label>
+          </div>
+          <div class="form-check">
           <label>Do you currently have small children in your home?</label><br>
-          <input type="radio" name="children" value="true" v-model="children">Yes<br>
-          <input type="radio" name="children" value="false" v-model="children">No<br>
+            <input class="radio-custom form-check-input" name="radio-group-children" type="radio" value="true" v-model="children">
+            <label class="radio-custom-label form-check-label">Yes</label>
+            <input class="radio-custom form-check-input" name="radio-group-children" type="radio" value="false" v-model="children">
+            <label class="radio-custom-label pl-2 form-check-label">No</label>
+          </div>
+          <h4 class="col-12 text-center pb-2">About You</h4>
+          <div class="col-12 form-check">
+          <label>Are you 18 or older?</label><br>
+            <input class="radio-custom form-check-input" name="radio-group-adult" type="radio" value="true" v-model="adult">
+            <label class="radio-custom-label form-check-label">Yes</label>
+            <input class="radio-custom form-check-input" name="radio-group-adult" type="radio" value="false" v-model="adult">
+            <label class="radio-custom-label pl-2 form-check-label">No</label>
+          </div>
+          <div class="col-12 form-check">
+          <label>Are you interested in volunteering with FoS?</label><br>
+            <input class="radio-custom form-check-input" name="radio-group-volunteer" type="radio" value="true" v-model="volunteer">
+            <label class="radio-custom-label form-check-label">Yes</label>
+            <input class="radio-custom form-check-input" name="radio-group-volunteer" type="radio" value="false" v-model="volunteer">
+            <label class="radio-custom-label pl-2 form-check-label">No</label>
+          </div>
+          <div class="col-12 form-check">
+          <label>Are you interested in Fostering for FoS?</label><br>
+            <input class="radio-custom form-check-input" name="radio-group-foster" type="radio" value="true" v-model="foster">
+            <label class="radio-custom-label form-check-label">Yes</label>
+            <input class="radio-custom form-check-input" name="radio-group-foster" type="radio" value="false" v-model="foster">
+            <label class="radio-custom-label pl-2 form-check-label">No</label>
+          </div>
+          <div class="col-12 form-check">
+          <label>Are you a member of USARK?</label><br>
+            <input class="radio-custom form-check-input" name="radio-group-usark" type="radio" value="true" v-model="usark">
+            <label class="radio-custom-label form-check-label">Yes</label>
+            <input class="radio-custom form-check-input" name="radio-group-usark" type="radio" value="false" v-model="usark">
+            <label class="radio-custom-label pl-2 form-check-label">No</label>
+          </div>
+          <div class="col-12 form-check">
+          <label>Do you have access to a veterinarian willing to work with exotic animals?</label><br>
+            <input class="radio-custom form-check-input" name="radio-group-exoticvet" type="radio" value="true" v-model="exoticVet">
+            <label class="radio-custom-label form-check-label">Yes</label>
+            <input class="radio-custom form-check-input" name="radio-group-exoticvet" type="radio" value="false" v-model="exoticVet">
+            <label class="radio-custom-label pl-2 form-check-label">No</label>
+          </div>
+          <div class="col-12 form-check">
+          <label>Have you ever owned a reptile, amphibian, or invertebrate before?</label><br>
+            <input class="radio-custom form-check-input" name="radio-group-everowned" type="radio" value="true" v-model="everOwned">
+            <label class="radio-custom-label form-check-label">Yes</label>
+            <input class="radio-custom form-check-input" name="radio-group-everowned" type="radio" value="false" v-model="everOwned">
+            <label class="radio-custom-label pl-2 form-check-label">No</label>
+          </div>
+             <h4 class="col-12 text-center pb-2">Adoption Details</h4>
+          <div class="col-12 form-check">
+          <label>Do you agree to seek veterinary care for your animal if an illness or injury were to occur?</label><br>
+            <input class="radio-custom form-check-input" name="radio-group-care" type="radio" value="true" v-model="care">
+            <label class="radio-custom-label form-check-label">Yes</label>
+            <input class="radio-custom form-check-input" name="radio-group-care" type="radio" value="false" v-model="care">
+            <label class="radio-custom-label pl-2 form-check-label">No</label>
+          </div>
+          <div class="col-12 form-check">
+          <label>Do you agree to present a permit if the animal you are adopting requires one?</label><br>
+            <input class="radio-custom form-check-input" name="radio-group-permit" type="radio" value="true" v-model="permit">
+            <label class="radio-custom-label form-check-label">Yes</label>
+            <input class="radio-custom form-check-input" name="radio-group-permit" type="radio" value="false" v-model="permit">
+            <label class="radio-custom-label pl-2 form-check-label">No</label>
+          </div>
+          <div class="col-12 form-check">
+          <label>Do you have a provision for your (future) pet in place should something happen to you?</label><br>
+            <input class="radio-custom form-check-input" name="radio-group-provisions" type="radio" value="true" v-model="provisions">
+            <label class="radio-custom-label form-check-label">Yes</label>
+            <input class="radio-custom form-check-input" name="radio-group-provisions" type="radio" value="false" v-model="provisions">
+            <label class="radio-custom-label pl-2 form-check-label">No</label>
+          </div>
+          <div class="col-12 form-check">
+          <label>Would you like Friends of Scales to send information on creating a bequest?</label><br>
+            <input class="radio-custom form-check-input" name="radio-group-bequest" type="radio" value="true" v-model="bequest">
+            <label class="radio-custom-label form-check-label">Yes</label>
+            <input class="radio-custom form-check-input" name="radio-group-bequest" type="radio" value="false" v-model="bequest">
+            <label class="radio-custom-label pl-2 form-check-label">No</label>
+          </div>
         </div>
-          <h4>About You</h4>
-          <div>
-            <label>Are you 18 or older?</label><br>
-            <input type="radio" name="adult" value="true" v-model="adult">Yes<br>
-            <input type="radio" name="adult" value="false" v-model="adult">No<br>
-          </div>
-           <div>
-            <label>Are you interested in volunteering with FoS?</label><br>
-            <input type="radio" name="volunteer" value="true" v-model="volunteer">Yes<br>
-            <input type="radio" name="volunteer" value="false" v-model="volunteer">No<br>
-          </div>
-           <div>
-            <label>Are you interested in Fostering for FoS?</label><br>
-            <input type="radio" name="foster" value="true" v-model="foster">Yes<br>
-            <input type="radio" name="foster" value="false" v-model="foster">No<br>
-          </div>
-          <div>
-            <label>Are you a member of USARK?</label><br>
-            <input type="radio" name="usark" value="true" v-model="usark">Yes<br>
-            <input type="radio" name="usark" value="false" v-model="usark">No<br>
-          </div>
-          <div>
-            <label>Do you have access to a veterinarian willing to work with exotic animals?</label><br>
-            <input type="radio" name="exotic_vet" value="true" v-model="exoticVet">Yes<br>
-            <input type="radio" name="exotic_vet" value="false" v-model="exoticVet">No<br>
-          </div>
-          <div>
-            <label>Have you ever owned a reptile, amphibian, or invertebrate before?</label><br>
-            <input type="radio" name="ever_owned" value="true" v-model="everOwned">Yes<br>
-            <input type="radio" name="ever_owned" value="false" v-model="everOwned">No<br>
-          </div>
-          <div>
-             <h4>Adoption Details</h4>
-          </div>
-          <div>
-            <label>Do you agree to seek veterinary care for your animal if an illness or injury were to occur?</label><br>
-            <input type="radio" name="care" value="true" v-model="care">Yes<br>
-            <input type="radio" name="care" value="false" v-model="care">No<br>
-          </div>
-           <div>
-            <label>Do you agree to present a permit if the animal you are adopting requires one?</label><br>
-            <input type="radio" name="permit" value="true" v-model="permit">Yes<br>
-            <input type="radio" name="permit" value="false" v-model="permit">No<br>
-          </div>
-          <div>
-            <label>Do you have a provision for your (future) pet in place should something happen to you?</label><br>
-            <input type="radio" name="provisions" value="true" v-model="provisions">Yes<br>
-            <input type="radio" name="provisions" value="false" v-model="provisions">No<br>
-          </div>
-          <div>
-            <label>Would you like Friends of Scales to send information on creating a bequest?</label><br>
-            <input type="radio" name="bequest" value="true" v-model="bequest">Yes<br>
-            <input type="radio" name="bequest" value="false" v-model="bequest">No<br>
-          </div>
-        <input type="submit" class="btn btn-primary" value="Submit">
-      </form>
-    </div>
+      <input type="submit" class="btn btn-primary" value="Submit">
+    </form>
   </div>
+</div>
 </template>
 
 <style>
